2868e37cdc9d8eb74809e9464260ad9e28cb0c57
SpareLink India <sparelink.ind@gmail.com>
2026-09-12 14:13:22 +0530

    feat: add server auth helper

---
+++ lib/auth-server.ts
@@ -0,0 +1,10 @@
+﻿import { auth } from "@/lib/auth";
+
+export async function getServerSession() {
+  return auth.api.getSession({
+    headers: await (async () => {
+      const { headers } = await import("next/headers");
+      return headers();
+    })(),
+  });
+}
